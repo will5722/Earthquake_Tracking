5d544ebc723afec52fc0d5408598743ad81f041f
Willam Lubenow <wkl22@humboldt.edu>
2021-11-19 12:57:43 -0800

    Changing approach to challenge

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,41 +1,22 @@
-//Past 7 days, all earthquakes.
+//Past 7 days, all earthquakes. Create map.
 var earthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-function createMap(earthquakes) {
 
-    //var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        //attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    //});
+var myMap = L.map("map", {
+    center: [32.7157, -117.1611],
+    zoom: 3
+});
 
-    var lightMap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: "Map data &copy; <a href=\'https://www.openstreetmap.org/\'>OpenStreetMap</a> contributors, <a href=\'https://creativecommons.org/licenses/by-sa/2.0/\'>CC-BY-SA</a>, Imagery © <a href=\'https://www.mapbox.com/\'>Mapbox</a>",
-        id: "mapbox/light-v10",
-        accessToken: API_KEY
-    });
-
-    var baseMaps = {
-        "Light Map": lightMap,
-    };
-
-    var overlayMap = {
-        "Earthquakes": earthquakes
-    };
-
-    var myMap = L.map("map", {
-        center: [32.7157, -117.1611],
-        zoom: 3,
-        layers: [lightMap, earthquakes]
-    });
-
-    L.control.layers(baseMaps, overlayMap, {
-        collapsed: false
-    }).addTo(myMap);
-}
-
-function CreateMarkers(response) {
+var lightMap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: "Map data &copy; <a href=\'https://www.openstreetmap.org/\'>OpenStreetMap</a> contributors, <a href=\'https://creativecommons.org/licenses/by-sa/2.0/\'>CC-BY-SA</a>, Imagery © <a href=\'https://www.mapbox.com/\'>Mapbox</a>",
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
+}).addTo(myMap);
 
 
 
-}
 
-createMap();
+
+
+
+
